5f1458ef85c17a2e1aab207ad61994b5e5d862e0
Can Baran Engin <canbaranengin1@gmail.com>
2022-08-02 11:25:10 +0300

    Odev1 class has been updated

--- Odev1/src/Odev1.java
+++ Odev1/src/Odev1.java
@@ -1,23 +1,29 @@
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
 public class Odev1 {
-
+    String repeat;
+    Scanner inp = new Scanner(System.in);
+    
     public void createStarShape(){
-        String repeat;
-
-        for(int i=1 ; i<8;i=i+2){
-            for(int j=i; j<8;j=j+2){
+        System.out.println("Enter the heigth value");
+        int heigth = inp.nextInt();
+        
+        for(int i=1 ; i<heigth*2;i=i+2){
+            for(int j=i; j<heigth*2;j=j+2){
                 System.out.printf(" ");
             }
             repeat="*".repeat(i);
             System.out.println(repeat);
 
         }
-        for(int k=5 ; k>0;k=k-2){
-            for(int z=k; z<9;z=z+2){
+        for(int k=heigth*2-3 ; k>0;k=k-2){
+            for(int z=k; z<heigth*2+1;z=z+2){
                 System.out.printf(" ");
             }
             repeat="*".repeat(k);
             System.out.println(repeat);
-
 
         }
 
